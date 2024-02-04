56a09cfc44b070828afa21e8a092250778459721
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-02-04 18:28:37 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState, useEffect } from 'react';
 import { ChatWindow } from "@/components/ChatWindow";
-import { checkDatabaseConnection } from "@/utils/database/checkDatabaseConnection";
 
 export default function Home() {
   const [dbConnected, setDbConnected] = useState(false);
