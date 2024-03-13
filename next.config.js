40d820bf1427a025b0dc57f105c9bce9976c72cd
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-03-13 21:13:35 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 module.exports = withBundleAnalyzer({})
+  experimental: {
+    webpackBuildWorker: true,
+  },
